ba3d271435971b2f483321a6993830e67759b344
Hai Nguyen <nhhaidee@gmail.com>
2021-09-07 14:11:25 -0500

    add basic test

--- tests/test_shicp.py
+++ tests/test_shicp.py
@@ -2,12 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from pathlib import Path
-import pandas as pd
+from os.path import exists
+from typer.testing import CliRunner
+
+from sequencing_coverage_plot.coverage_plot.cli import app
+
+runner = CliRunner()
+
+dirpath = Path(__file__).parent
+input_ref = dirpath/'data/nCoV-2019.reference.fasta'
+input_genbank = dirpath/'data/sequence_sars_cov2.gb'
 
 
-def inc(x):
-    return x + 1
-
-
-def test_answer():
-    assert inc(4) == 5
+def test_cli():
+    assert input_ref.exists()
+    assert input_genbank.exists()
+    result = runner.invoke(app)
+    result = runner.invoke(app)
+    assert result.exit_code != 0
+    assert 'Error: Missing option' in result.output
+    help_result = runner.invoke(app, ['--help'])
+    assert help_result.exit_code == 0
+    assert 'Show this message and exit.' in help_result.output
