d82e3546fb3d275bde5de9f8f3cd9d67de268985
Hai Nguyen <nhhaidee@gmail.com>
2021-10-14 15:46:22 -0500

    add jtest suites

--- tests/wgscovplot.test.js
+++ tests/wgscovplot.test.js
@@ -1,13 +1,13 @@
-import {median} from '../wgscovplot/tmpl/js/wgscovplot.js'
+import {median} from '../wgscovplot/tmpl/js/wgscovplot.js';
 
 function sum(a, b) {
   return a + b;
 }
 
-var arr = [1, 4 , 7 , 9]
+var arr = [1, 4 , 7 , 9];
 
 test('Median of an array [1, 4 , 7 , 9]', () => {
-  expect(median(arr).toBe(5.5));
+  expect(median(arr)).toBe(5.5);
 })
 
 test('adds 1 + 2 to equal 3', () => {
