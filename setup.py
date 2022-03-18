73a7cd2660831486ff75dcdeb7b8b0e1d723a8bb
Peter Kruczkiewicz <peter.kruczkiewicz@gmail.com>
2022-03-18 14:32:11 -0500

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nhhaidee/sequencing_coverage_plot',
-    version='0.1.0',
+    version='0.1.1',
     zip_safe=False,
 )
