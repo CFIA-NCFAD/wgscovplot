9e01de09ca1c45be3ef4fe44a4276085f10c1cf4
Hai Nguyen <nhhaidee@gmail.com>
2022-09-12 15:07:37 -0500

    fix test missing package

--- wgscovplot/prepare_data.py
+++ wgscovplot/prepare_data.py
@@ -8,6 +8,7 @@ import edlib
 
 from Bio.SeqFeature import SeqFeature
 from jinja2 import Environment, FileSystemLoader
+from Bio import SeqIO
 from itertools import cycle
 from .resources import gene_feature_properties
 from .colors import color_pallete, AmpliconColour
